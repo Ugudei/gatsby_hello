3c9493a648b36e9b5d1d784b5325566e1686e682
Ugudei T <ugudeit@mac-machine.local>
2021-02-02 21:20:54 -0700

    gatsby hello

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: 'navy' }}>
+      <Header headerText="About Baby Yoda" />
+      <Header headerText="He is so cute" />
+      <p> His name is Grogu.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return ( <div style={{ color: 'blue', fontSize: '72px'}}>
+    <Link to="/contact">Contact</Link>
+    <h1>Hello world!</h1> 
+    <p>love baby</p>
+    <img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2020%2F11%2Fbaby-yoda-disney-plus-mandalorian-name-grogu-info-001.jpg?q=75&w=800&cbr=1&fit=max" alt="" />
+    </div>
+  );
 }
